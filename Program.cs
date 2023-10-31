75f51fef7e8005abd02bc5e615f5909f66088e76
Mary <mashenka.koryagina@inbox.ru>
2023-10-31 15:37:14 +0300

    Entering data into an array

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿Console.Write("Enter the number of array elements: ");
 int m = Convert.ToInt32(Console.ReadLine());
 string [] stringArray = new string [m];
+
+void array(string [] stringArray)
+{
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+     Console.WriteLine($"Введите {i+1} элемент массива");
+     stringArray[i] = Console.ReadLine();
+  }
+}
